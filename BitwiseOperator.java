9b6caf09b02b4b082a5ab1dc998110a1585a0f90
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-11-24 18:01:03 +0530

    Create BitwiseOperator.java

---
+++ BitwiseOperator.java
@@ -0,0 +1,24 @@
+public class BitwiseOperator {
+  public static void main(String[] args) {
+    // Bitwise Operator: These operators are used to change the bits(Bit
+    // Manipulation)
+    // Bit: Smallest unit of data (0, 1)
+
+    // 0110101001
+
+    // Bitwise OR(|)
+    // Bitwise AND(&)
+    // Bitwise XOR(^)
+    // Bitwise Complement(~)
+    // Bitwise Left Shift Operator(<<)
+    // Bitwise Right Shift Operator(>>)
+
+    int a = 3; // ->
+    int b = 5; // ->
+
+    int result = a | b;
+
+    System.out.println(result);
+  }
+
+}
