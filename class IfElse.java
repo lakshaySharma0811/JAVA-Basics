87193f1b85cb27cac1173a39029afd0d3f0720d5
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-11-25 17:46:30 +0530

    Create  class IfElse.java

---
+++ class IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+
+  public static void main(String[] args) {
+
+    int age = 20;
+
+    if (age == 10) {
+      System.out.println("I am inside if block");
+    } else if (age < 18) {
+      System.out.println("I am inside else if block");
+    } else {
+      System.out.println("I am inside else block");
+    }
+  }
+}
