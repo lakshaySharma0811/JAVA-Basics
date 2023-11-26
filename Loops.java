1ffad5aee54c9913859c7ec4209d4ba3beeb1a20
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-11-26 20:47:29 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,39 @@
+public class Loops {
+
+  public static void main(String[] args) {
+    // Loops: Repetation
+
+    // 1. while
+    // 2. do while
+    // 3. for
+
+    // 1. while loop:
+    // int num = 0;
+    // while (num < 10) {
+    // System.out.println("Hello from while loop: " + num++);
+    // }
+
+    // int print_table_of = 2;
+    // int counter = 1;
+
+    // while (counter <= 10) {
+    // System.out.println(print_table_of + " * " + counter + " = " + print_table_of
+    // * counter++);
+    // }
+
+    // 2. do while
+    // int num = 1000;
+    // do {
+    // System.out.println("Number is: " + num++);
+    // } while (num < 10);
+
+    // 3. For
+
+    // for (initial_value, condition, increment){s
+    // for (int a = 1; a < 10; a++) {
+    // System.out.println("A is: " + a);
+    // }
+
+  }
+
+}
