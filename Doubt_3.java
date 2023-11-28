1a7b62aef816b27e412e58f14886efe98612c8a9
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-11-29 01:20:38 +0530

    Create Doubt_3.java

---
+++ Doubt_3.java
@@ -0,0 +1,38 @@
+public class Doubt_3 {
+  public static void main(String[] args) {
+    int n = 4;
+
+    for (int i = 10; i >= 7; i--) {
+      for (int j = 1; j <= n; j++) {
+        System.out.print("1 ");
+      }
+      for (int j = 1; j <= n; j++) {
+        System.out.print("2 ");
+      }
+      for (int j = 1; j <= n; j++) {
+        System.out.print("3 ");
+      }
+      System.out.println();
+    }
+  }
+}
+
+// *
+// * *
+// * * *
+// * * * *
+
+// * * * *
+// * * *
+// * *
+// *
+
+// *
+// * *
+// * * *
+// * * * *
+
+// * * * *
+// * * *
+// * *
+// *
