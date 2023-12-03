8d800f1b92f3615bb2b9f08fb3e8d068817ae397
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-03 12:56:01 +0530

    Update and rename MyClass.java to MyClass_1.java

--- MyClass_1.java
+++ MyClass_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MyClass {
+public class MyClass_1 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int input = sc.nextInt();
