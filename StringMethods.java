2bad412991a6abcf1fe792ad70f268976865235e
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-06 01:30:55 +0530

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,87 @@
+import java.util.List;
+
+public class StringMethods {
+  public static void main(String[] args) {
+    // ==============================================contains
+    // String str = "Hello World!";
+    // System.out.println(str.contains("World"));
+
+    // ==============================================split
+    // String str = "My name is Harsh";
+
+    // String -> String[]
+    // String[] strs = str.split(" ");
+    // System.out.println(str);
+    // printStringArray(strs);
+
+    // ==============================================join
+    // List[] -> String
+    // List<String> strs = List.of("I", "Love", "Java", "ABC");
+    // String joinedString = String.join("ahrshsadsa", strs);
+    // System.out.println(joinedString);
+
+    // ==============================================trim
+
+    // String str = " My name is Harsh ";
+    // String trimmedString = str.trim();
+    // System.out.println("Length of original string: " + str.length());
+    // System.out.println("Length of trimmed string: " + trimmedString.length());
+
+    // ==============================================replace
+    // String str = "My nahme his Harsh";
+    // String updatedString = str.replace("h", "Lakshay");
+    // System.out.println("Original string: " + str);
+    // System.out.println("Updated string: " + updatedString);
+
+    // ==============================================replaceFirst
+    // String str = "My nahme his Harsh";
+    // String updatedString = str.replaceFirst("h", "Lakshay");
+    // System.out.println("Original string: " + str);
+    // System.out.println("Updated string: " + updatedString);
+
+    // ==============================================startsWith
+    // String str = "My nahme his Harsh";
+    // System.out.println(str.startsWith("his"));
+
+    // ==============================================endsWith
+    // String str = "My nahme his Harsh";
+    // System.out.println(str.endsWith("Harshdasdaw"));
+
+    // ==============================================isEmpty, isBlank
+    // String str = "";
+    // System.out.println(str.isBlank());
+    // System.out.println(str.isEmpty());
+
+    // ===============================================Basic Arrays
+    // String[] data = {};
+    // int[] nums = { 1, 2, 3 };
+    // float[] floats = { 1.2f, 3.4f };
+    // char[] chars = { 'A', 'B' };
+    // boolean[] bools = { true, false, true, true };
+    // printIntArray(nums);
+
+  }
+
+  // static void printStringArray(String[] arr) {
+  // for (int i = 0; i < arr.length; i++) {
+  // System.out.print(arr[i] + " ");
+  // }
+  // }
+
+  // static void printIntArray(int[] arr) {
+  // for (int i = 0; i < arr.length; i++) {
+  // System.out.print(arr[i] + " ");
+  // }
+  // }
+}
+
+// contains() -> true/false
+// split()
+// join()
+// trim()
+// replace()
+// replaceFirst()
+// startsWith()
+// endsWith()
+// isEmpty()
+// isBlank()
