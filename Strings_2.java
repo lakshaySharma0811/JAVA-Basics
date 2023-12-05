db2b45cfeff9ea549c55202b4afb9f91e3ff36ba
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-05 20:07:00 +0530

    Create Strings_2.java

---
+++ Strings_2.java
@@ -0,0 +1,44 @@
+public class Strings_2 {
+  public static void main(String[] args) {
+
+    // String x = "HeLlO";
+    // String y = "hello";
+
+    // System.out.println(x.equals(y));
+    // System.out.println((x.toLowerCase()).equalsIgnoreCase(y.toUpperCase()));
+
+    // ======================================================================
+    // String msg = "Hello %s! How are you. Roll no: %s";
+    // String updatedMsg = String.format(msg, "Lakshy");
+    // System.out.println(updatedMsg);
+
+    // =======================================================================
+    // String str = "Hello from Harsh";
+    // String newStr = str.substring(11, 16);
+
+    // System.out.println("Original String: " + str);
+    // System.out.println("Sub String: " + newStr);
+
+    // =======================================================================
+    // String str = "Hello from Harsh";
+
+    // for (int i = str.length() - 1; i >= 0; i--) {
+    // char x = str.charAt(i);
+    // System.out.print(x);
+    // }
+
+  }
+}
+
+// String (In Built Methods):
+
+// concat
+// length
+// charAt
+// toLowerCase
+// toUpperCase
+// equals
+// equalsIgnoreCase
+// valueOf
+// String.format()
+// substring
