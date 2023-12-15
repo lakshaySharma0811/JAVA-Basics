7913fee183ab0e44dca010b0e04c1d293f5f62f3
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-15 22:46:27 +0530

    Delete 04-12-23 directory

--- 04-12-23/entity/Animal.java
+++
@@ -1,9 +0,0 @@
-package entity;
-
-public class Animal {
-
-  public void xyz() {
-    System.out.print("Hello from Animal");
-  }
-
-}
--- 04-12-23/items/stationary/Pen.java
+++
@@ -1,5 +0,0 @@
-package items.stationary;
-
-public class Pen {
-
-}
